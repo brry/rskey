3602e2010e802261ff0985beeaaaa13d11156b6b
brry <berryboessenkool@hotmail.com>
2017-05-19 16:13:10 +0200

    head_addin: typos corrected

--- R/head_addin.R
+++ R/head_addin.R
@@ -1,7 +1,7 @@
-#' @title Rstudio head on F$
+#' @title Rstudio head on F4
 #' @description Rstudio addin to show the \code{\link{head}} of a highlighted object.
 #' Instead of an object name, code creating data can be highlighted as well.
-#' It can be very useful to bind this addin to F$, for example.
+#' It can be very useful to bind this addin to F4, for example.
 #' @return Output from \code{\link{head}}.
 #' @author Berry Boessenkool, \email{berry-b@@gmx.de}, May 2017
 #' @export
@@ -9,7 +9,7 @@
 #' @references Heavily borrowed from \url{https://github.com/digital-dharma/RStudioAddIns}
 #' @seealso \code{\link{selectobject}}, \code{\link{str_addin}}
 #' @examples
-#' # Go to Addins - browse Addins - Keyboard shortcuts - map "structure of an object" to F4
+#' # Go to Addins - browse Addins - Keyboard shortcuts - map "head of an object" to F4
 #'
 #' @param obj Some R object. DEFAULT: Rstudio addin selected code
 #'
