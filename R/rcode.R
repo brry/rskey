ae354eef176bb22666ca3a25619e610c32f3138a
brry <berryboessenkool@hotmail.com>
2019-11-25 22:25:56 +0100

    rcode addin added to package

---
+++ R/rcode.R
@@ -0,0 +1,25 @@
+#' @title wrap Rnw source code in "\\rcode{}"
+#' @author Berry Boessenkool, \email{berry-b@@gmx.de}, Nov 2019
+#' @seealso \code{\link{selectobject}}
+#' @importFrom rstudioapi getActiveDocumentContext insertText
+#' @export
+#'
+rcode <- function()
+{
+# Extract highlighted text from Active Document
+so_context <- try(rstudioapi::getActiveDocumentContext(), silent=TRUE)
+if(inherits(so_context, "try-error")) stop("rstudioapi::getActiveDocumentContext failed.\n",
+"This can happen if you clicked in a code_browser instead of a script window.", call.=FALSE)
+so_text <- so_context$selection[[1]]$text
+
+# Error Checking to Ensure Text is Selected
+if(nchar(so_text) == 0) stop("Nothing is highlighted in the RStudio Source Editor. ",
+                          "Please ensure a function argument section is highlighted.",
+                            call.=FALSE)
+
+out <- paste0("\\rcode{", so_text, "}")
+rstudioapi::insertText(text=out, id=so_context$id)
+message("\\rcode is added to ", so_context$path)
+return(invisible(out))
+}
+
--- R/setKeyboardBindings.R
+++ R/setKeyboardBindings.R
@@ -101,7 +101,8 @@ rskey::summary_addin   | F8
 rskey::dim_addin       | F9
 rskey::class_addin     | F10
 rskey::plot_addin      | F11
-rskey::hist_addin      | F12")
+rskey::hist_addin      | F12
+rskey::rcode           | Ctrl+,")
 if(nrow(setkeys)>0) new <- new[!new$key %in% setkeys[!setkeys$remove,"key"]   , ]
 
 if(nrow(new)>0)
