01c4d55c8d8c284d89e5f5007bcac504d39a46ee
brry <berryboessenkool@hotmail.com>
2024-09-09 12:32:38 +0200

    pycheck now uses unittest

--- R/pycheck.R
+++ R/pycheck.R
@@ -1,17 +1,6 @@
 #' @title run python exercise development test
 #' @author Berry Boessenkool, \email{berry-b@@gmx.de}, Jan 2023
 #' @seealso \code{\link{selectobject}}
-#' @importFrom rstudioapi getActiveDocumentContext
-#' @importFrom berryFunctions checkFile
 #' @export
 #'
-pycheck <- function()
-{
-rstudioapi::documentSave()
-p <- rstudioapi::documentPath()
-p <- substr(basename(p),1,3)
-p <- paste0(p,"testrun.py")
-berryFunctions::checkFile(p)
-message("running python ", p)
-system2("python", p)
-}
+pycheck <- function() codeoceanR:::py_local_score()
